ea393c18d98cdc43566d835f23c6951f5a4be7e7
TVIGes <khdv66@gmail.com>
2020-10-25 21:31:53 +0500

    Generate Spring Boot project

---
+++ src/main/java/edu/njmsd/stonksmonkey/StonksMonkeyApplication.java
@@ -0,0 +1,13 @@
+package edu.njmsd.stonksmonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonksMonkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonksMonkeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/njmsd/stonksmonkey/StonksMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.njmsd.stonksmonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonksMonkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
